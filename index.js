45307bd84a4eee662e001eeadfe6ead0218870f9
Gemilang Karunia Gusti <125884682+gilangGusti@users.noreply.github.com>
2023-03-21 23:14:09 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+// Level 3 Tugas 1
+let a = "1";
+let b = "2";
+a = "2";
+b = "1";
+console.log(a, b);
+// Level 3 Tugas 2 No. 1 
+// Algoritma adalah proses atau serangkaian aturan yang harus diikuti dalam perhitungan atau operasi pemecahan masalah, terutama oleh komputer. Dengan kata lain, semua susunan logis yang diurutkan berdasarkan sistematika tertentu dan digunakan untuk memecahkan suatu masalah.
+// Level 3 Tugas 2 No. 2
+console.log("Algoritma Memasak Mie Instan");
+console.log("1. Siapkan Air dan Rebus air sampai mendidih");
+console.log("2. Jika sudah Mendidih Masukan Mie tunggu hingga mie cukup matang");
+console.log("3. Setelah mie matang. tiriskan mie untuk beberapa menit dan buang air bekas rebusan mie");
+console.log("4. Setelah ditiriskan,Masukan ke dalam mangkok atau piring, dan tuangkan air hangat untuk menjadi kuah mir");
+console.log("5. Taburkan Bumbu penyedap mie instan");
+console.log("6. Mie Siap di santap, MANTABBB!!!!!")
